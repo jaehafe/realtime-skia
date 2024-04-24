af70c93d8d4acb227bbc05f87effea7efbe5ed1a
Jaeha Lee <wogkdkrm112@gmail.com>
2024-04-24 22:48:34 +0900

    feat: Drawing Board

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,48 @@
-import { Stack, Link } from 'expo-router';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View, StyleSheet } from 'react-native';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+export default function Page() {
+  const router = useRouter();
+  const [channel, setChannel] = useState('1');
 
-export default function Home() {
+  const onJoin = () => {
+    router.navigate(`/channel/${channel}`);
+  };
+
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <View>
+      <TextInput
+        value={channel}
+        onChangeText={setChannel}
+        style={styles.input}
+        inputMode="numeric"
+      />
+      <TouchableOpacity onPress={onJoin} style={styles.button}>
+        <Text style={styles.text}>Join</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: '#808080',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 12,
+    margin: 8,
+  },
+  button: {
+    backgroundColor: '#2E8A58',
+    borderRadius: 8,
+    padding: 12,
+    margin: 8,
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
